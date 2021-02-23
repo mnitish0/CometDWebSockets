18f70505094682b62b1d4504ec6701e4556b493e
mnitish0 <63292357+mnitish0@users.noreply.github.com>
2021-02-24 08:34:22 +1000

    Update index.js

--- src/Function2/index.js
+++ src/Function2/index.js
@@ -1,3 +1,3 @@
 const AWS = require('aws-sdk'); const ddb = new AWS.DynamoDB.DocumentClient();
-function addConnectionId(connectionId) { return ddb.delete({ TableName: 'ConnectedClients', Key: { connectionid: connectionId, }, }).promise(); }
+function addConnectionId(connectionId) { return ddb.delete({ TableName: 'cometdwebsockets-harrysanboxenvironment-ConnectedClients-1KKM7J5EOSQVN', Key: { connectionid: connectionId, }, }).promise(); }
 exports.handler = (event, context, callback) => { const connectionId = event.requestContext.connectionId; addConnectionId(connectionId).then(() => { callback(null, { statusCode: 200, }) }); }
